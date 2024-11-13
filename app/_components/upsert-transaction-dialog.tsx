008af3470e1bc2daabec82f597c5c29e01b9f6d4
ascefpam <ascefpam@gmail.com>
2024-11-12 21:14:54 -0300

    feat: fix number amount defaultValue

--- app/_components/upsert-transaction-dialog.tsx
+++ app/_components/upsert-transaction-dialog.tsx
@@ -85,7 +85,7 @@ const UpsertTransactionDialog = ({
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: defaultValues ?? {
-      amount: 50,
+      amount: 1,
       category: TransactionCategory.OTHER,
       date: new Date(),
       name: "",
